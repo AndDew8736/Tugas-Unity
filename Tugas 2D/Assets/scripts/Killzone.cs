2259375cccff36d3604283f0060ee7f9302d0965
AndDew8736 <amob8736@gmail.com>
2025-06-02 00:27:07 +0700

    Polish main menu + add win screen + add decorations

--- Tugas 2D/Assets/scripts/Killzone.cs
+++ Tugas 2D/Assets/scripts/Killzone.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Killzone : MonoBehaviour
 {
+    [Serialize] private string sceneToLoad;
     public Transform player, destination;
     public GameObject Player;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Player") //if player, tp to start
+        if (other.gameObject.tag == "Player") //if player, restart level
         {
-            other.transform.position = destination.position;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
         else if (other.gameObject.tag == "GroundCheck")
         {
-            //does nothing
+            //do nothing
         }
         else //delete object
         {
